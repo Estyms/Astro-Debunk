d10223c931768c09a07dfeecbd291f75784e399c
Evann <estym.yt@gmail.Com>
2019-07-31 21:38:03 +0200

    Website Done

---
+++ Files/index.js
@@ -0,0 +1,54 @@
+var urlParams = new URLSearchParams(window.location.search);
+if (urlParams.get('prenom') != null && urlParams.get('prenom').length  >  0){
+    $.getJSON( "/Files/Classement.json", function( data ) {
+        name = urlParams.get('prenom').normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+
+        $.getJSON( "Files/Classement.json", function( data ) {
+            
+            var items = [];
+            $.each( data, function( key, val ) {
+                if (key.toLowerCase() == name.toLowerCase()) {
+                    console.log("HEY");
+                    items.push("<h4>" + val.classement +"# "+ key + "<h4/><h5>" + val.score + " Points</h5>" );
+                }
+            });
+           if (items.length == 0) items.push("<h4> Ce prénom n'est pas dans la base de données.");
+            $( "<div>", {
+              "class": "end collection",
+              "style": "margin-bottom:50px",
+              html: items.join( "" )
+            }).appendTo( "body" );
+
+            $.getJSON( "Files/Classement.json", function( data ) {
+                var items = [];
+                $.each( data, function( key, val ) {
+                  items.push( "<li class='collection-item'><h6>" + val.classement +"# "+ key + "<h6/>" + val.score + " Points </li>" );
+                });
+               
+                $( "<ul>", {
+                  "class": "collection",
+                  html: items.join( "" )
+                }).appendTo( "body" );
+              });
+          });
+    });
+    
+} else{
+    $.getJSON( "Files/Classement.json", function( data ) {
+        var items = [];
+        $.each( data, function( key, val ) {
+          items.push( "<li class='collection-item'><h6>" + val.classement +"# "+ key + "<h6/>" + val.score + " Points </li>" );
+        });
+       
+        $( "<ul>", {
+          "class": "collection",
+          html: items.join( "" )
+        }).appendTo( "body" );
+      });
+}
+
+
+
+history.pushState({
+    id: 'homepage'
+}, 'Calc-Name', "/");   
